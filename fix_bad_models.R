94ee1fa3e211faf4e39afee25230106d12cead2e
pbonney <peter.bonney@gmail.com>
2015-11-11 14:47:05 -0500

    got fix_bad_models functions working

--- fix_bad_models.R
+++ fix_bad_models.R
@@ -1,15 +1,38 @@
 library(parallel)
 library(neuralnet)
+library(data.table)
 
 logfile <- "model_err.txt"
 
-try.model.f <- function(m.t) {
-    if (!is.numeric(m.t$result.matrix[1])) {
+try.model.f <- function(m.temp) {
+    if (!is.numeric(m.temp$result.matrix[1])) {
         return(FALSE)
     }
-    p.test <- try(compute(m.t,data.frame(px=0,pz.ratio=0))$net.result)
+    p.test <- try(compute(m.temp,data.frame(px=0,pz.ratio=0))$net.result)
     if (class(p.test) == "try-error") {
         return(FALSE)
     }
     return(TRUE)
+}
+
+try.model.wrapper.f <- function(filepath) {
+    result <- FALSE
+    if(file.exists(filepath)) {
+        load(file=filepath)
+        m1 <- m.t
+        rm(m.t)
+        result <- try.model.f(m1)
+    }
+    return(result)
+}
+
+find.bad.models.f <- function(dir) {
+    log <- paste(dir,logfile,sep="/")
+    if(file.exists(log)) { file.remove(log) }
+
+    models.l <- dir(path=dir, pattern="*rda")
+    dt.result <- data.table(file=paste(dir,models.l,sep="/"))
+    dt.result$test <- mcmapply(try.model.wrapper.f,dt.result$file,
+                               mc.cores=getOption("mc.cores",20L))
+    return(dt.result[dt.result$test==FALSE,])
 }
