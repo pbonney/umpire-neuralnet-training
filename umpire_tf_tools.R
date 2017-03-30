50dc260212c2dba38479e2cb40b0065f790f70a9
pbonney <peter.bonney@gmail.com>
2017-03-29 22:53:20 -0400

    add a simpler NN model

--- umpire_tf_tools.R
+++ umpire_tf_tools.R
@@ -153,6 +153,8 @@ m.label.validate <- as.integer(data.frame(dt.validate$s.f)[,1])
 
 sess <- tf$InteractiveSession()
 
+# Model 1
+
 x <- tf$placeholder(tf$float32, shape(NULL, 2L), name='x_0')
 
 D_h <- 2L
@@ -160,48 +162,48 @@ D_h <- 2L
 W_0 <- weight_variable(shape(2L, 2L), name='W_0')
 b_0 <- bias_variable(shape(1L, 2L), name='b_0')
 y_0 <- tf$matmul(x, W_0) + b_0
-# a_0 <- tf$nn$relu(y_0)
-a_0 <- y_0
+a_0 <- tf$nn$sigmoid(y_0)
 
 W_h1 <- weight_variable(shape(2L, D_h), name='W_h1')
 b_h1 <- bias_variable(shape(1L, D_h), name='b_h1')
 y_h1 <- tf$matmul(a_0, W_h1) + b_h1
-a_h1 <- tf$nn$relu(y_h1)
+a_h1 <- tf$nn$sigmoid(y_h1)
 
 W_h2 <- weight_variable(shape(2L, D_h), name='W_h2')
 b_h2 <- bias_variable(shape(1L, D_h), name='b_h2')
 y_h2 <- tf$matmul(a_0, W_h2) + b_h2
-a_h2 <- tf$nn$relu(y_h2)
+a_h2 <- tf$nn$sigmoid(y_h2)
 
 W_h3 <- weight_variable(shape(2L, D_h), name='W_h3')
 b_h3 <- bias_variable(shape(1L, D_h), name='b_h3')
 y_h3 <- tf$matmul(a_0, W_h3) + b_h3
-a_h3 <- tf$nn$relu(y_h3)
+a_h3 <- tf$nn$sigmoid(y_h3)
 
 W_h4 <- weight_variable(shape(2L, D_h), name='W_h4')
 b_h4 <- bias_variable(shape(1L, D_h), name='b_h4')
 y_h4 <- tf$matmul(a_0, W_h4) + b_h4
-a_h4 <- tf$nn$relu(y_h4)
+a_h4 <- tf$nn$sigmoid(y_h4)
 
 a_h_comb <- tf$concat(c(a_h1, a_h2, a_h3, a_h4), 1L)
 
 W_out <- weight_variable(shape(4L * D_h, 2L), name='W_out')
 b_out <- bias_variable(shape(1L, 2L), name='b_out')
 y_out <- tf$matmul(a_h_comb, W_out) + b_out
-a_out <- tf$nn$relu(y_out)
+a_out <- tf$nn$sigmoid(y_out)
 
-y <- tf$nn$softmax(a_out)
+y <- a_out
+# y <- tf$nn$softmax(a_out)
 
 y_ <- tf$placeholder(tf$float32, shape(NULL, 2L))
 
 print("Define optimizer")
-cross_entropy <- tf$reduce_mean(-tf$reduce_sum(y_ * tf$log(y), reduction_indices=1L))
+# loss <- tf$reduce_mean(-tf$reduce_sum(y_ * tf$log(y), reduction_indices=1L)) # cross-entropy
+loss <- tf$reduce_mean(tf$losses$sigmoid_cross_entropy(logits=y, multi_class_labels=y_))
 # loss <- tf$reduce_mean((y - y_) ^ 2, reduction_indices=1L)
 
 optimizer <- tf$train$AdamOptimizer(1e-4)
 # optimizer <- tf$train$GradientDescentOptimizer(0.5)
-train_step <- optimizer$minimize(cross_entropy)
-# train_step <- optimizer$minimize(loss)
+train_step <- optimizer$minimize(loss)
 
 x_in <- m.data.train
 y_in <- tf$one_hot(m.label.train, 2L)$eval()
@@ -235,3 +237,61 @@ print(result)
 sess$run(tf$local_variables_initializer())
 result_auc <- sess$run(auc, feed_dict = dict(x = x_test, y_ = y_test))
 print(result_auc)
+
+# model 2
+
+x2 <- tf$placeholder(tf$float32, shape(NULL, 2L), name='x2')
+
+D_h <- 2L
+
+W2 <- weight_variable(shape(2L, 10L), name='W2')
+b2 <- bias_variable(shape(1L, 10L), name='b2')
+y2_0 <- tf$matmul(x2, W2) + b2
+a2 <- tf$nn$sigmoid(y2_0)
+
+Wo <- weight_variable(shape(10L, 1L), name='Wo')
+bo <- bias_variable(shape(1L, 1L), name='bo')
+y2 <- tf$matmul(a2, Wo) + bo
+
+y2_ <- tf$placeholder(tf$float32, shape(NULL, 1L))
+
+loss2 <- tf$reduce_mean(tf$losses$sigmoid_cross_entropy(logits=y2, multi_class_labels=y2_))
+# loss2 <- tf$reduce_mean((y - y_) ^ 2, reduction_indices=1L)
+
+optimizer2 <- tf$train$AdamOptimizer(1e-4)
+# optimizer2 <- tf$train$GradientDescentOptimizer(0.5)
+train_step2 <- optimizer2$minimize(loss2)
+
+x2_in <- m.data.train
+y2_in <- as.matrix(m.label.train)
+
+# correct_prediction2 <- tf$equal(tf$round(tf$cast(y2, tf$float32)), tf$round(tf$cast(y2_, tf$float32)))
+correct_prediction2 <- abs(y2 - y2_) < 0.5
+accuracy2 <- tf$reduce_mean(tf$cast(correct_prediction2, tf$float32))
+
+a2 = y2
+b2 = y2_
+auc2 = tf$contrib$metrics$streaming_auc(a2, b2)
+
+sess$run(tf$global_variables_initializer())
+
+print("Train!!!")
+for (i in 1:200) {
+  sess$run(train_step2,
+           feed_dict = dict(x2 = x2_in, y2_ = y2_in))
+  if (i %% 20 == 0) {
+		train_accuracy2 <- accuracy2$eval(feed_dict = dict(x2 = x2_in, y2_ = y2_in))
+    cat(sprintf("step %d, training accuracy %g\n", i, train_accuracy2))
+	}
+}
+
+print("Evaluate!")
+x2_test <- m.data.test
+y2_test <- as.matrix(m.label.test)
+
+result2 <- sess$run(accuracy2, feed_dict = dict(x2 = x2_test, y2_ = y2_test))
+print(result2)
+
+sess$run(tf$local_variables_initializer())
+result_auc2 <- sess$run(auc2, feed_dict = dict(x2 = x2_test, y2_ = y2_test))
+print(result_auc2)
